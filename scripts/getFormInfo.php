439cfbc21400659f5032a6d70b388f7ad2255e35
dvakarandashavnos <dvakarandashavnos@gmail.com>
2016-09-12 17:15:44 +0300

    1
    1

---
+++ scripts/getFormInfo.php
@@ -0,0 +1,18 @@
+﻿<?php
+$first_name = $_REQUEST['first_name'];
+$last_name = $_REQUEST['last_name'];
+$email = $_REQUEST['email'];
+$facebook_url = $_REQUEST['facebook_url'];
+$twitter_handle = $_REQUEST['twitter_handle'];
+?>
+<html>
+<head>
+<link href="../../css/phpMM.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+Имя: <?php echo $first_name . " " . $last_name ?><br>
+Фейсбук: <a href="<?php echo $facebook_url ?>">Ваш фейсбук</a><br>
+Твиттер: <?php echo $twitter_handle ?><br>
+Email: <?php echo $email ?>
+</body>
+</html>

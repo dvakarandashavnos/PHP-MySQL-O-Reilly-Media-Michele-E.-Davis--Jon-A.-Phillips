bb0f3d9f95d4fac15753be03c145942a8912bcd5
dvakarandashavnos <dvakarandashavnos@gmail.com>
2016-09-12 17:16:28 +0300

    Revert "Revert "summary""
    This reverts commit a44d4152545848c7040cd71f0015cd7cef5f1eb8.

---
+++ getFormInfo.php
@@ -0,0 +1,15 @@
+<?php
+$first_name = $_REQUEST['first_name'];
+$last_name = $_REQUEST['last_name'];
+$email = $_REQUEST['email'];
+$facebook_url = $_REQUEST['facebook_url'];
+$twitter_handle = $_REQUEST['twitter_handle'];
+?>
+<html>
+<head>
+<link href="../../css/phpMM.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<!-- Существующий HTML-код -->
+</body>
+</html>
